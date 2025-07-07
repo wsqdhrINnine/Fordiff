5f71992e7377335e6ebcef03178debc3ebad0dc1
wsqdhrINnine <140699744+wsqdhrINnine@users.noreply.github.com>
2025-07-07 21:58:50 +0800

    Update Kernel_Fission.cu

--- Kernel_Fission.cu
+++ Kernel_Fission.cu
@@ -1,19 +1,12 @@
-template <typename T, typename T_ACC>
-__global__ void GammaBetaBackwardCUDAKernel(
-   int64_t M, int64_t N, const T* dY, const T* X, const T_ACC* mean, const T_ACC* rstd, T* dg, T* db) {
-  alignas(sizeof(double)) extern __shared__ char s_data1[];
-  T_ACC* s_data_typed = reinterpret_cast<T_ACC*>(&s_data1);
-  T_ACC* s_dg;
-  T_ACC* s_db;
-  const int64_t j = ((int64_t) blockIdx.x) * blockDim.x + threadIdx.x;
-  T_ACC dg_sum = 0;
-  T_ACC db_sum = 0;
-  if (j < N) {
-    constexpr int unroll_factor = 8;
-    T_ACC mean_reg;
-    T_ACC rstd_reg;
-    T dY_reg;
-    T X_reg;
-    // Main Loop
-      ...
-  }
+template<typename T, typename T_ACC>
+void LaunchGammaBetaBackwardCUDAKernel(
+    const T* dY_data, const T* X_data, const T_ACC* mean_data, const T_ACC* rstd_data, int64_t M, int64_t N, Tensor* dgamma, Tensor* dbeta, cudaStream_t cuda_stream) {
+  constexpr int block_dim_x = 32;
+  const int sm_count = at::cuda::getCurrentDeviceProperties()->multiProcessorCount;
+  if (M > 64 * 1024 && N / block_dim_x < sm_count / 2) {
+  //Compute partial sums for the gradient in the first pass
+  //Then do a .sum(0) to do a final reduction.
+   ...
+} else {
+// Normal main Loop
+}
