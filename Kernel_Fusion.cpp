a8fa326c563e70b861fbc5a09d6575fa66d7dddd
wsqdhrINnine <140699744+wsqdhrINnine@users.noreply.github.com>
2025-07-07 20:11:16 +0800

    Update Kernel_Fusion.cpp

--- Kernel_Fusion.cpp
+++ Kernel_Fusion.cpp
@@ -1,8 +1,7 @@
-auto max_val = (-input).clamp_min_(0);
 if (pos_weight.defined()) {
-    // pos_weight need to be broadcasted, thus mul(target) is not inplace.
-    auto log_weight = (pos_weight - 1).mul(target).add_(1);
-    loss = (1 - target).mul_(input).add_(log_weight.mul_(((-max_val).exp_().add_((-input - max_val).exp_())).log_().add_(max_val)));
-} else {
-    loss = (1 - target).mul_(input).add_(max_val).add_((-max_val).exp_().add_((-input -max_val).exp_()).log_());
-}
+      // pos_weight need to be broadcasted, thus mul(target) is not inplace.
+      auto log_weight = (pos_weight - 1).mul(target).add_(1);
+      loss = (1 - target).mul_(input).sub_(log_weight.mul_(at::log_sigmoid(input)));
+  } else {
+      loss = (1 - target).mul_(input).sub_(at::log_sigmoid(input));
+  }
