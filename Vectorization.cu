a89fc21e9ce6d87283ed996b64cae70bde0af5ce
wsqdhrINnine <140699744+wsqdhrINnine@users.noreply.github.com>
2025-07-07 15:47:15 +0800

    Update Vectorization.cu

--- Vectorization.cu
+++ Vectorization.cu
@@ -1,4 +1,4 @@
-constexpr bool kPack4 = (sizeof(T) == 2);
+q23
 constexpr int kElemsPerVec = kPack4 ? 4 : 1;
 for (IndexType vecLinear = (blockIdx.x * blockDim.x + threadIdx.x) * kElemsPerVec;
     vecLinear < totalSize;
