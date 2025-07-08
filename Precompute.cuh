b685660a791d8e9bb98146258cc199adb7ba7bc7
wsqdhrINnine <140699744+wsqdhrINnine@users.noreply.github.com>
2025-07-08 08:49:45 +0800

    Update Precompute.cuh

--- Precompute.cuh
+++ Precompute.cuh
@@ -1,7 +1,9 @@
-if (row < num_rows) {
+const bool row_exists = row < num_rows;
+if (row_exists) {
   ...
-  for (int s = num_threads_x, d = 1; s >= 1; s >>= 1, d <<= 1) {
-    if (row < num_rows && threadIdx.x < s) {
-      int offset = (2 * threadIdx.x + 1) * d - 1;
-      binary_op_update(row_buf[offset], row_buf[offset + d], row_idx_buf[offset], row_idx_buf[offset + d], binary_op);
-    }}}
+  for (uint32_t s = 1; s <= num_threads_x; s <<= 1) {
+    if (row_exists) {
+      uint32_t a = (threadIdx.x / s) * (2 * s) + s;
+      uint32_t ti = a + (threadIdx.x % s);
+      uint32_t si = a - 1;
+      binary_op_update(row_buf[si], row_buf[ti], row_idx_buf[si], row_idx_buf[ti], binary_op);}}}
