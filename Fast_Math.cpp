094ad301a4e5acf672cc4541a45a6e4dbdde390c
wsqdhrINnine <140699744+wsqdhrINnine@users.noreply.github.com>
2025-07-08 12:48:05 +0800

    Update Fast_Math.cpp in PR#92116

--- Fast_Math.cpp
+++ Fast_Math.cpp
@@ -1,5 +1,4 @@
-Vectorized<float> one(1.0);
-Vectorized<float> two(2.0);
+Vectorized<float> inv_log_2_vec(inv_log_2);
 Vectorized<float> m0 = maximum(a0, b0);
 Vectorized<float> m1 = maximum(a1, b1);
-a0 = Vectorized<float>::blendv(m0 + (one + two.pow((a0 - b0).abs().neg())).log2(), a0, (a0 == b0) & (a0.abs() == inf));
+a0 = Vectorized<float>::blendv(m0 + (a0 - b0).abs().neg().exp2().log1p() * inv_log_2_vec, a0, (a0 == b0) & (a0.abs() == inf));
