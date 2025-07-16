8e660795311332dfd5b65cdf5ba9f440f97d7fb3
wsqdhrINnine <140699744+wsqdhrINnine@users.noreply.github.com>
2025-07-16 14:57:09 +0800

    Update Kernel_Fission.cu

--- Kernel_Fission.cu
+++ Kernel_Fission.cu
@@ -1,5 +1,8 @@
-template <typename T, typename T_ACC>
-__global__ void GammaBetaBackwardCUDAKernel(
-   int64_t M, int64_t N, const T* dY, const T* X, const T_ACC* mean, const T_ACC* rstd, T* dg, T* db) {
-  ...
-  if (j < N) {// Main Loop}
+template<typename T, typename T_ACC>
+void LaunchGammaBetaBackwardCUDAKernel(
+    const T* dY_data, const T* X_data, const T_ACC* mean_data, const T_ACC* rstd_data, int64_t M, int64_t N, Tensor* dgamma, Tensor* dbeta, cudaStream_t cuda_stream) {
+   ...
+  if (M > 64 * 1024 && N / block_dim_x < sm_count / 2) {
+  //Compute partial sums for the gradient in the first pass
+  //Then do a .sum(0) to do a final reduction.
+} else {// Normal main Loop}
