4b6223f62af10eef13110cdb2a9eef9b201750cf
wsqdhrINnine <140699744+wsqdhrINnine@users.noreply.github.com>
2025-07-17 22:21:29 +0800

    Update Kernel_Fusion.cpp

--- Kernel_Fusion.cpp
+++ Kernel_Fusion.cpp
@@ -1,6 +1,12 @@
-Tensor indices_mult_cpu = at::from_blob(
-  indices_mult_cpu_vec.data(),
-  indices.options().device(kCPU).dtype(kLong));
-Tensor indices_mult = indices_mult_cpu.to(indices.device(), /*non_blocking=*/false);
-return indices.mul(indices_mult).sum(0);
-
+KernelLauncher<kernel_t>::launch(iter,
+    // NOTE: capture by value required by CUDA
+    [=] FUNCAPI (int64_t nnz_idx) -> int64_t {
+    const auto* RESTRICT ptr_indices_dim = ptr_indices + nnz_idx * indices_nnz_stride;
+    auto hash = static_cast<int64_t>(0);
+    for (int64_t dim = 0; dim < sparse_dim; ++dim) {
+      const auto dim_hash_coeff = hash_coeffs[dim];
+      const auto dim_index = ptr_indices_dim[dim * indices_dim_stride];
+      hash += dim_index * dim_hash_coeff;
+    }
+    return hash;
+});
