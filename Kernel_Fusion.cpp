c5a584ba0c077d2343996d0663827e68c4101f1a
wsqdhrINnine <140699744+wsqdhrINnine@users.noreply.github.com>
2025-07-17 22:21:11 +0800

    Update Kernel_Fusion.cpp

--- Kernel_Fusion.cpp
+++ Kernel_Fusion.cpp
@@ -1,5 +1,6 @@
-    Tensor indices_mult_cpu = at::from_blob(
-      indices_mult_cpu_vec.data(),
-      indices.options().device(kCPU).dtype(kLong));
-    Tensor indices_mult = indices_mult_cpu.to(indices.device(), /*non_blocking=*/false);
-    return indices.mul(indices_mult).sum(0);
+Tensor indices_mult_cpu = at::from_blob(
+  indices_mult_cpu_vec.data(),
+  indices.options().device(kCPU).dtype(kLong));
+Tensor indices_mult = indices_mult_cpu.to(indices.device(), /*non_blocking=*/false);
+return indices.mul(indices_mult).sum(0);
+
