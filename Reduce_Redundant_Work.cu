1657aa300f645a0126cca3d70bad27ef9d0f7845
wsqdhrINnine <140699744+wsqdhrINnine@users.noreply.github.com>
2025-07-17 21:16:46 +0800

    Update Reduce_Redundant_Work.cu

--- Reduce_Redundant_Work.cu
+++ Reduce_Redundant_Work.cu
@@ -1,5 +1,5 @@
 at::parallel_for(0, N, 0, [&](int64_t start, int64_t end) {
       for (const auto n : c10::irange(start, end)) {
-        scalar_t *grid_ptr_N = grid_ptr + n * grid_sN;
-        scalar_t *inp_ptr_N = inp_ptr + n * inp_sN;
+        const scalar_t *grid_ptr_N = grid_ptr + n * grid_sN;
+        const scalar_t *inp_ptr_N = inp_ptr + n * inp_sN;
         ...}
